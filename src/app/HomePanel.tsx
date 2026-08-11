9d445ff69f4248780434ff482050dabd7cb93e94
Wilson Yu <wilsonyu657@gmail.com>
2026-08-11 01:40:53 -0400

    Small formatting change to points

--- src/app/HomePanel.tsx
+++ src/app/HomePanel.tsx
@@ -4,11 +4,18 @@ import { SecondaryDiv, TertiaryDiv } from "./reusable/DivPresets";
 
 export default function HomePanel() {
   return (
-    <SecondaryDiv className="relative rounded-2xl flex flex-col justify-start items-center p-10 g-5">
-      <TertiaryDiv className="relative flex flex-col justify-center items-center rounded-2xl p-5 g-5 ">
-        <h2 className="text-xl">Points</h2>
-        <p className="text-center font-bold text-4xl">0</p>
+    <SecondaryDiv className="relative rounded-2xl flex flex-col justify-start items-center p-10 gap-5">
+      <TertiaryDiv className="relative flex flex-col justify-center items-center rounded-2xl min-w-50 p-5 ">
+        <h1 className="text-xl text-center font-bold">Points</h1>
+        <p className="text-center font-bold text-8xl">{0}</p>
       </TertiaryDiv>
+      <TertiaryDiv className="relative flex flex-col justify-center items-center rounded-2xl p-10 gap-10"></TertiaryDiv>
     </SecondaryDiv>
+  );
+}
+
+function Divider() {
+  return (
+    <div className="relative bg-font-tertiary w-[0.1rem] opacity-50 rounded-full my-1" />
   );
 }
